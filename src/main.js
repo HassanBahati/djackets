baa1e7ed7faa547e7685bd194db66f67b913f226
HassanBahati <mukisabahati@gmail.com>
2021-05-23 16:24:25 +0300

    added axios config in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// setting default api to be consumed 
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+createApp(App).use(store).use(router, axios).mount('#app')
