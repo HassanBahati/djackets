33ccdc7b132d5202784fe4f7109ce138a3e6f27a
HassanBahati <mukisabahati@gmail.com>
2021-05-23 17:53:19 +0300

    added cart functionalitty

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,31 @@ export default createStore({
   },
   // anchroynous for the states becuase we cant just change them directly 
   mutations: {
+    // adding funtinality to add to cart 
+    // func to store items in local browser storage 
+    initializeStore(state){
+      if (localStorage.getItem('cart')){
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      }else{
+        // if item doesnt exist in ls create it 
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item){
+      // var to help check if it exists 
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      // if length is larger than 0 then item exists in cart       
+      if(exists.length){ 
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      }else{
+        // if doesnt exist add it 
+        state.cart.items.push(item)
+      }
+
+      // when browser is refreshed restor default cart 
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    },
   },
   // asncy func to change state 
   actions: {
