8ee2114e2250ef6b136a04c17140eeb3269b374a
HassanBahati <mukisabahati@gmail.com>
2021-05-23 17:23:53 +0300

    added image to prod details page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -45,19 +45,28 @@ export default {
   name: "Product",
   data() {
     return {
-        product: {},
-        quantity: 1
+      product: {},
+      quantity: 1,
     };
   },
-  mounted(){
-      this.getProduct()
+  mounted() {
+    this.getProduct();
   },
   methods: {
-      getProduct(){
-          const category_slug = this.$route.params.category_slug
-          const product_slug = this.$route.params.product_slug
-      }
-  }
+    getProduct() {
+      const category_slug = this.$route.params.category_slug;
+      const product_slug = this.$route.params.product_slug;
+
+      axios
+        .get(`/api/v1/products/${category_slug}/${product_slug}`)
+        .then((response) => {
+          this.product = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
