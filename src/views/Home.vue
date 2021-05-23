aeb2f25bdf6c2fa99ff853da30353b090eb1398d
HassanBahati <mukisabahati@gmail.com>
2021-05-23 15:56:07 +0300

    added prods section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="home">
-   <section class="hero is-medium is-dark mb-6">
-     <div class="hero-body has-text-centered">
-       <p class="title mb-6">
-         Welcome to Djackets
-       </p>
-       <p class="subtitle">
-         The best jackets store online
-       </p>
-     </div>
-   </section>
+    <!-- hero landing section  -->
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcome to Djackets</p>
+        <p class="subtitle">The best jackets store online</p>
+      </div>
+    </section>
+    <!-- prods section  -->
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  components: {
-  } 
-}
+  name: "Home",
+  components: {},
+};
 </script>
