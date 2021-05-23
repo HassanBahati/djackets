046884fe6d34fab0f0afc343c289ae57041cce5b
HassanBahati <mukisabahati@gmail.com>
2021-05-23 17:32:46 +0300

    added state

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,19 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // state-variable for informations 
   state: {
+    cart: {
+      items: [],
+    },
+    isAuthenticated: false,
+    token: '',
+    isLoaded: false
   },
+  // anchroynous for the states becuase we cant just change them directly 
   mutations: {
   },
+  // asncy func to change state 
   actions: {
   },
   modules: {
