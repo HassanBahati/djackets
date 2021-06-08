25244fbceaaab76b11ef8ab00a79a6baa777d79d
HassanBahati <mukisabahati@gmail.com>
2021-06-08 13:07:44 +0300

    edited cart

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,31 +1,57 @@
 <template>
-    <tr>
-        <td>
-            <router-link :to="item.product.get_absolute_url">{{item.product.name}}</router-link>
-            
-        </td>
-            <td>${{item.product.price}}</td>
-            <td>{{item.quantity}}</td>
-            <td>${{getItemTotal(item).toFixed(2)}}</td>
-            <td><button class="delete"></button></td>
-    </tr>
+  <tr>
+    <td>
+      <router-link :to="item.product.get_absolute_url">{{
+        item.product.name
+      }}</router-link>
+    </td>
+    <td>${{ item.product.price }}</td>
+    <td>{{ item.quantity }}
+        &nbsp;<a @click="decrementQuantity(item)">-</a>
+        &nbsp;<a @click="incrementQuantity(item)">+</a>
+    </td>
+    <td>${{ getItemTotal(item).toFixed(2) }}</td>
+    <td><button class="delete" @click="removeFromCart(item)"></button></td>
+  </tr>
 </template>
 
 <script>
 export default {
-    name: 'CartItem',
-    props: {
-        initialItem: Object
+  name: "CartItem",
+  props: {
+    initialItem: Object,
+  },
+  data() {
+    return {
+      item: this.initialItem,
+    };
+  },
+  methods: {
+    getItemTotal(item) {
+      return item.quantity * item.product.price;
     },
-    data(){
-        return{
-            item: this.initialItem
+    decrementQuantity(item){
+        item.quantity -= 1
+
+// not to go below 1 , remove completely from cart 
+        if(item.quantity === 0){
+            this.$emit('removeFrontCart', item)
         }
+
+        this.updateCart()
     },
-    methods: {
-        getItemTotal(item){
-            return item.quantity * item.product.price
-        }
+    incrementQuantity(item){
+        item.quantity += 1
+        this.updateCart()
+    },
+    updateCart(){
+        localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
+    },
+    removeFromCart(item){
+       this.$emit('removeFromCart', item)
+
+       this.updateCart()
     }
-}
+  },
+};
 </script>
