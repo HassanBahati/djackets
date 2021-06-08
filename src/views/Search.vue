441f41b220d0b4637a424b7844868b6200478083
HassanBahati <mukisabahati@gmail.com>
2021-06-08 12:08:48 +0300

    added serach queries

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -31,6 +31,32 @@ export default {
     },
     mounted(){
         document.title='Search | Djackets'
+
+        // get info from url 
+        let uri = window.location.search.substring(1)
+        let params = new URLSearchParams(uri)
+
+        if(params.get('query')){
+            this.query = params.get('query')
+
+            this.performSearch()
+        }
+
+    },
+    methods: {
+        async performSearch(){
+            this.$store.commit('setIsLoading', true)
+
+            await axios.post('/api/v1/products/search/', {'query': this.query})
+            .then(response=>{
+                this.products = response.data
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+
+            this.$store.commit('setIsLoading', false)
+        }
     },
 }
 </script>
